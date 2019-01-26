6cacc429822a105c787f79420f8ae166990697e8
Alexander-AJ-Berman <alexander_berman@brown.edu>
2019-01-26 12:37:25 -0500

    route handlers for create room and join

--- app.js
+++ app.js
@@ -8,18 +8,16 @@ router.get('/',function(req,res){
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
-});
-
-router.get('/sitemap',function(req,res){
-  res.sendFile(path.join(__dirname+'/sitemap.html'));
+router.get('/create', function(req, res){
+  res.sendFile(path.join(__dirname + '/views/create.html'));
+  // Continued logic for creating a room with database entries
 });
 
 // Add someone to a room with synchro logic
 router.get('/join', function(req, res) {
-  // LOGIC
-}
+  res.sendFile(path.join(__dirname + '/views/join.html'));
+  // Continued logic for joining a room, with db entries
+});
 
 //add the router
 app.use('/', router);
