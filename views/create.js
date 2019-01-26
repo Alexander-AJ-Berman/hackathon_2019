ecf27d6d74727ec7eb61a53db9d2946acb23a3a1
parthenopoulosdimitris <dimitrios_parthenopoulos@brown.edu>
2019-01-26 12:44:37 -0500

    Create a session page

---
+++ views/create.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+	$("#form").submit(function(event){
+	    event.preventDefault();
+	    var session = document.getElementById("session").value;
+	    var password1 = document.getElementById("password1").value;
+	    var password2 = document.getElementById("password2").value;
+
+	    if(password1.length < 6 || password1.length > 25) {
+	    	alert("Password must be between 6-25 characters");
+	    }
+	    else if(password1 != password2) {
+	    	alert("Passwords don't match!");
+	    }
+	    else {
+	    	$.post('/app.js', {
+	    		session: session, password: password,
+			}, function(data){
+                window.location.href = "/";
+            });
+
+
+
+	    }
+
+
+  });
+});
