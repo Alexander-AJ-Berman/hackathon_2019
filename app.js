9dcd05bab93a043268b25479c6fffe31658fe9bc
Alexander-AJ-Berman <alexander_berman@brown.edu>
2019-01-26 12:00:41 -0500

    join arm

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ router.get('/sitemap',function(req,res){
   res.sendFile(path.join(__dirname+'/sitemap.html'));
 });
 
+// Add someone to a room with synchro logic
+router.get('/join', function(req, res) {
+  // LOGIC
+}
+
 //add the router
 app.use('/', router);
 app.listen(process.env.port || 3000);
