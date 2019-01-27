63001c7ab0af2f7854b5dd1aa723b3dc959b1512
kei-kinoshita <keiichiro_kinoshita@brown.edu>
2019-01-26 19:25:47 -0500

    merge boyz

--- app.js
+++ app.js
@@ -8,14 +8,206 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Handles spotify login and Auth
-function spotify_login() {
-  // TODO; for K
-}
+var request = require('request'); // "Request" library
+var querystring = require('querystring');
+var cookieParser = require('cookie-parser');
+let client_id =  "8fdf389a4342424b8c52c8e8456653ae";
+let client_secret = "fb6eb63063074d0bb69844de3a2a03c3";
+let redirect_uri = 'http://localhost:3000/callback'; // Your redirect uri
+var SpotifyWebApi = require('spotify-web-api-node');
+
+let spotifyApi = new SpotifyWebApi({
+  clientId: client_id,
+  clientSecret: client_secret
+});
+
+let chris_api = new SpotifyWebApi({
+  clientId: client_id,
+  clientSecret: client_secret
+});
+
+
+let aj_api = new SpotifyWebApi({
+  clientId: client_id,
+  clientSecret: client_secret
+});
+
+let kei_access = '';
+// let aj_access = 'BQBSyywnCKzATbX97N6Jx3xfz2WZEt30gSjoNnd9CG9Q8v58e2oI8zKh0scVotxpv5PcT2u0hpLN9172mJBBye85hV3R9rrlS74Lb_7oInSVAsCUTC5xrRMQkZYomzDitJisapvbH4Tn7yiMSd97nlFm4QTTctHPcK7O';
+let chris_access = 'BQDPCVlEFHh75lokgjb_ObyvH1ZHvDrvgR6lwFeKbnVORo_0w1pozDvjgclc_zn-2BOimXsGUNC9FY-PWlxGADP4--AT0MlGOL8Gq-N55PcSth_PIRujKMdE9JqsBI94NzGzK7g1G9Z6UA5pKcvcpkhY2CQ';
+
+let aj_access = 'BQCFCKUuayrWy3B1acGtvCpKOv0dxvO8uGrExoaw0Xak6pyqj-m1MlAIku9Cdftycl0qIHL6Oxt8lZWj45wfqWSHC5Ymn35sqwHln86I_xHzdkX7ZTmo5LhC5CFsRvMUbv-8TWgri7cG-abeGm1bb5j027qP4ray6GQ7SU9lBQ';
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+var generateRandomString = function(length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+var stateKey = 'spotify_auth_state';
+
+app.use(express.static(__dirname + '/public'))
+   .use(cookieParser());
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email user-modify-playback-state';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state,
+      show_dialog: "true"
+    }));
+});
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  var code = req.query.code || null;
+  var state = req.query.state || null;
+  var storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    var authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        var access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        kei_access = body.access_token;
+
+        var options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body);
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  var refresh_token = req.query.refresh_token;
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      });
+    }
+  });
+});
+
+router.get('/omt', function(req,res) {
+    console.log('onemoretime');
+    console.log('kei_access: ' + kei_access);
+    spotifyApi.setAccessToken(kei_access);
+    chris_api.setAccessToken(chris_access);
+    aj_api.setAccessToken(aj_access);
+
+    spotifyApi.play(
+      {"context_uri": "spotify:album:7D2NdGvBHIavgLhmcwhluK",  "offset": {
+    "position": 1} })
+      .then(function(data) {
+        console.log('PLAYING ONE MORE TIME ON MAIN ACCT!');
+      }, function(err) {
+        console.log('Something went wrong!', err);
+    });
+
+    aj_api.play(
+      {"context_uri": "spotify:album:7D2NdGvBHIavgLhmcwhluK",  "offset": {
+    "position": 1} })
+      .then(function(data) {
+        console.log('__playing one more time!');
+      }, function(err) {
+        console.log('Something went wrong 4 aj!', err);
+    });
+
+
+    chris_api.play(
+      {"context_uri": "spotify:album:7D2NdGvBHIavgLhmcwhluK",  "offset": {
+    "position": 1} })
+      .then(function(data) {
+        console.log('__playing one more time!');
+      }, function(err) {
+        console.log('Something went wrong 4 aj!', err);
+    });
+
+});
+
+// // Handles spotify login and Auth
+// function spotify_login() {
+//   // TODO; for K
+// }
 
 router.get('/',function(req,res){
-  spotify_login();
+  // spotify_login();
   res.sendFile(path.join(__dirname+'/views/index.html'));
   //__dirname : It will resolve to your project folder.
 });
@@ -69,8 +261,8 @@ router.post('/create_user', function(req, res) {
 // Add someone to a room with synchro logic
 router.get('/join', function(req, res) {
   var room_names = [
-    { name: "AJ's Room" }, 
-    { name: "Kei's Room" }, 
+    { name: "AJ's Room" },
+    { name: "Kei's Room" },
     { name: "Dimitris's Room" }
   ];
 
@@ -89,6 +281,7 @@ router.get('/playback', function(req, res) {
 // set up static routing
 app.use(express.static(path.join(__dirname, 'public')));
 //add the router
+app.use('omt', router);
 app.use('/', router);
 app.use('/create', router);
 app.use('join', router);
@@ -130,4 +323,3 @@ var roomSchema = new mongoose.Schema({
 
 var User = mongoose.model('User', userSchema);
 var Room = mongoose.model('Room', roomSchema);
-
